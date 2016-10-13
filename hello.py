860ecdf2d02209a437f608ff72e292b2f253ff43
TingleHaro <947559133@qq.com>
2016-10-13 23:43:27 +0800

    Got some more further changes.

--- hello.py
+++ hello.py
@@ -2,7 +2,5 @@ print("Hello")
 print("Changes in another")
 print("World")
 print("More changes")
-<<<<<<< HEAD
-=======
 print("Still more changes")
->>>>>>> d0cfd6dacc1c12c060eb89080af391cf58526674
+
